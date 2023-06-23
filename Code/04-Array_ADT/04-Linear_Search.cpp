3c9209cf0774c697be64666b01b2295b130ab94d
Arpit <arpitsengar99@gmail.com>
2023-06-23 16:26:40 +0530

    added linear search

---
+++ Code/04-Array_ADT/04-Linear_Search.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    int i;
+    printf("\nElements are\n");
+    for (i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i]);
+}
+
+void swap(int* x, int* y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int LinearSearch(Array arr, int key)
+{
+    for (int i = 0; i < arr.length; i++)
+        if (key == arr.A[i])
+            return i;
+    return -1;
+}  
+
+int LinearSearch2(Array* arr, int key)
+{
+    int i;
+    for (i = 0; i < arr->length; i++)
+    {
+        if (key == arr->A[i])
+        {
+            swap(&arr->A[i], &arr->A[0]);
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    Array arr = { {2,3,4,5,6}, 10, 5 };
+
+    printf("Index (simple approach) : %d\n", LinearSearch(arr, 5));
+    printf("Index (using move to head) : %d", LinearSearch2(&arr, 3));
+
+    return 0;
+}
+
